7ac9163781b0dd51478a424ace2b6eca4b8c077c
Stasovss <stsv.sts@gmail.com>
2023-03-14 19:57:19 +0300

    Update helper.py

--- lib/helper.py
+++ lib/helper.py
@@ -2,11 +2,7 @@ import string
 import random
 
 
-
 def generate_random_string(length):
     letters = string.ascii_lowercase
     rand_string = ''.join(random.choice(letters) for i in range(length))
     return rand_string
-
-
-print(generate_random_string(12))
